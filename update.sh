b5fe7255b7d07ecfc36e87bac04b9bedee73bbb9
Lucky <lucky2077@mailverse.xyz>
2023-09-22 16:30:15 +0800

    updatee script support choose version

--- update.sh
+++ update.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 
-git subtree pull --prefix=atomicals-js  https://github.com/atomicals/atomicals-js.git master --squash
+version=${1:-"master"}
+
+git subtree pull --prefix=atomicals-js  https://github.com/atomicals/atomicals-js.git ${version} --squash
